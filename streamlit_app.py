f0f7e7c061e7d17a5f114c061583b0dbfea83763
SurajSitoula2000 <177130642+SurajSitoula2000@users.noreply.github.com>
2025-07-10 23:00:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,12 +12,12 @@ with st.expander('Data'):
   df
 
   st.write('**X**')
-  X = df.drop('species', axis=1)
-  X
+  X_raw = df.drop('species', axis=1)
+  X_raw
 
   st.write('**y**')
-  y = df.species
-  y
+  y_raw = df.species
+  y_raw
 
 with st.expander('Data Visualization'):
   st.scatter_chart(data=df, x='culmen_length_mm', y='body_mass_g', color='species')
@@ -40,12 +40,17 @@ with st.sidebar:
           'body_mass_g': body_mass_g,
           'sex': gender}
   input_df = pd.DataFrame(data, index=[0])
-  input_penguins = pd.concat([input_df, X], axis=0)
+  input_penguins = pd.concat([input_df, X_raw], axis=0)
 
-# Encode
-  encode = ['island', 'sex']
-  df_penguins = pd.get_dummies(input_penguins, prefix=encode)
-  input_row = df_penguins[:1]
+# Encode x
+encode = ['island', 'sex']
+df_penguins = pd.get_dummies(input_penguins, prefix=encode)
+input_row = df_penguins[:1]
+
+# Encode y
+  
+
+
 
 with st.expander('Input features'):
   st.write('**Input penguins**')

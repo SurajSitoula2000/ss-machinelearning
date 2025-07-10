c9808634f8e4845409bf60cdffc5aafecfb5835a
SurajSitoula2000 <177130642+SurajSitoula2000@users.noreply.github.com>
2025-07-10 18:13:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,5 @@ st.title('🐱‍💻 Machine Learning App')
 
 st.info('This is app builds a machine learning model!')
 
-df = pd.read_csv(r"https:\\github.com\SurajSitoula2000\ss-machinelearning\blob\master\penguins_size.csv")
+df = pd.read_csv('https://raw.githubusercontent.com/SurajSitoula2000/ss-machinelearning/refs/heads/master/penguins_size.csv')
 df
-

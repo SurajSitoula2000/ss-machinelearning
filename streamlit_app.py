0450d41bfbecc2e4fb2b439cbc63e1be8cf6773e
SurajSitoula2000 <177130642+SurajSitoula2000@users.noreply.github.com>
2025-07-11 13:16:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,6 +53,7 @@ with st.expander('Input features'):
 # Encode x
 encode = ['island', 'sex']
 df_penguins = pd.get_dummies(input_penguins, prefix=encode)
+
 X = df_penguins[1:]
 input_row = df_penguins[:1]
 
@@ -74,7 +75,7 @@ with st.expander('Data preparation'):
 # Model training and inference
 ## Train the ML Model
 clf = RandomForestClassifier()
-clf.fit(df_penguins, y)
+clf.fit(X, y)
 
 ## Apply model to make prediction
 prediction = clf.predict(input_row)
